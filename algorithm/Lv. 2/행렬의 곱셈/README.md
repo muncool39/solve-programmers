b53e31aaa97cde1e899ee95567d601182b6044fd
siwon <muncool39@gmail.com>
2024-05-08 11:17:23 +0900

    solve: 행렬의 곱셈
    연습문제

---
+++ algorithm/Lv. 2/행렬의 곱셈/README.md
@@ -0,0 +1,6 @@
+## 문제 링크
+https://school.programmers.co.kr/learn/courses/30/lessons/12949
+
+## 관련
+**행렬의 곱셈 개념**
+- https://mathbang.net/562
---
+++ algorithm/Lv. 2/행렬의 곱셈/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+  public int[][] solution(int[][] arr1, int[][] arr2) {;
+      int[][] answer = new int[arr1.length][arr2[0].length];
+      for(int i=0;i<arr1.length;i++) {
+          for(int j=0;j<arr2[0].length;j++) {
+              for(int n=0;n<arr1[0].length;n++) {
+                  answer[i][j] += arr1[i][n] * arr2[n][j];
+              }
+          }
+      }
+      return answer;
+  }
+}
