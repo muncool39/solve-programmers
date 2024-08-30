98b624795950e306c4f47685a7a6b190a5b764b5
siwon <muncool39@gmail.com>
2024-08-30 16:45:19 +0900

    solve: 2개 이하로 다른 비트
    월간 코드 챌린지 시즌2

---
+++ algorithm/Lv. 2/2개 이하로 다른 비트/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public long[] solution(long[] nums) {
+        long[] ans = new long[nums.length];
+        int idx=0;
+        for(long num:nums) {
+            for(long i=1;;i<<=1) {
+                if((num&i)==0) {
+                    num|=i;
+                    num^=(i>>1);
+                    break;
+                }
+            }
+            ans[idx++]=num;
+        }
+        return ans;
+    }
+}
