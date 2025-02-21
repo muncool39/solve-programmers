d88ce5f2de4422bc63340ecbef44970d36431f17
siwon <muncool39@gmail.com>
2025-02-22 04:38:35 +0900

    solve: [SELECT] 대장균의 크기에 따라 분류하기 2

---
+++ SQL/Lv. 3/대장균의 크기에 따라 분류하기 2/Solution.sql
@@ -0,0 +1,12 @@
+SELECT ID, CASE (NRANK) 
+                WHEN 1 THEN "CRITICAL"
+                WHEN 2 THEN "HIGH"
+                WHEN 3 THEN "MEDIUM"
+                WHEN 4 THEN "LOW"
+            END AS COLONY_NAME
+FROM (
+    SELECT ID, 
+    NTILE(4) OVER (ORDER BY SIZE_OF_COLONY DESC) AS NRANK
+    FROM ECOLI_DATA
+) T
+ORDER BY ID
